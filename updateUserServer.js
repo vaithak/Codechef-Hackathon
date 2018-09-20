8c784f3f856964cca7070cb9a5cbed58c722a13d
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2018-09-20 23:47:00 +0530

    Fix updateUserServer error

--- updateUserServer.js
+++ updateUserServer.js
@@ -10,60 +10,54 @@ const app = express()
 mongoose.connect(keys.mongodb.dbURI,{ useNewUrlParser: true }, function() {
     console.log('connected to mongodb');
 });
-var userlist=[];
-var i=0;
-var myuser;
+
 User.findOne({codechefId :keys.codechef.username.toLowerCase()}).then(function(currentUser){
-	myuser=currentUser;
+	var myUser=currentUser;
 	console.log("Okay");
+  start(myUser['refreshToken']);
 });
 
-setInterval(function(){
-	User.find(function(err,docs){
-		console.log(docs);
-		for(var i in docs)
-		{
-			userlist.push(docs[i]["codechefId"]);
-		}
-	});
-},1000*60*60*6);
+function start(myUserRefreshToken){
+  setInterval(function(){
+    var userlist=[];
+    User.find(function(err,docs){
+      // console.log(docs);
+      for(var i=0; i<docs.length; i++)
+      {
+        userlist.push(docs[i]["codechefId"]);
+      }
 
-setInterval(function(){
-	if(i<userlist.length)
-	{
-		var username=userlist[i];
-		i++;
-		refreshToken.refreshAccessToken(myuser.refreshToken,keys.codechef.username.toLowerCase()).then(function(accessToken){
-	    var options = {
-	      method: 'GET',
-	      uri: 'https://api.codechef.com/users/'+username,
-	      headers: {
-	         'Accept': 'application/json',
-	         'Authorization': 'Bearer ' + accessToken
-	      },
-	      json: true // Automatically parses the JSON string in the response
-	    };
-	    request(options)
-	    .then(function(result){
-	    	var rankings  =  result['result']['data']['content']['rankings'];
-	    	var ratings   =  result['result']['data']['content']['ratings'];
-	    	var institute =  result['result']['data']['content']['organization'];
-        var band      = result['result']['data']['content']['band'];
-	    	User.findOneAndUpdate({'codechefId':username},{$set:{rating:ratings,ranking:rankings,institute:institute,band:band}}, function(err, doc){
-	    		console.log("Updated "+username);
-	    	});;
-	    });
-	   });
-		if(i==userlist.length)
-		{
-			userlist=[];
-		}
-	}
-	else
-	{
-		i=0;
-	}
-},1000*60);
+      console.log(userlist);
+
+      for(var i=0 ; i<userlist.length; i++)
+      {
+        var username=userlist[i];
+        refreshToken.refreshAccessToken(myUserRefreshToken,keys.codechef.username.toLowerCase()).then(function(accessToken){
+          var options = {
+            method: 'GET',
+            uri: 'https://api.codechef.com/users/'+username,
+            headers: {
+               'Accept': 'application/json',
+               'Authorization': 'Bearer ' + accessToken
+            },
+            json: true // Automatically parses the JSON string in the response
+          };
+          request(options)
+          .then(function(result){
+            var rankings  =  result['result']['data']['content']['rankings'];
+            var ratings   =  result['result']['data']['content']['ratings'];
+            var institute =  result['result']['data']['content']['organization'];
+            var band      = result['result']['data']['content']['band'];
+            User.findOneAndUpdate({'codechefId':username},{$set:{rating:ratings,ranking:rankings,institute:institute,band:band}}, function(err, doc){
+              console.log("Updated "+username);
+            });;
+          });
+         });
+      }
+    });
+  },1000*60*60*10);
+}
+
 
 
 const port = process.env.port || 8090;
