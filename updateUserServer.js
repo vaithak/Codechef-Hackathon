86de47e5beb3b703df1da19f2e16836a7f3e6d99
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2018-09-22 11:33:12 +0530

    Change update user

--- updateUserServer.js
+++ updateUserServer.js
@@ -4,7 +4,7 @@ const keys = require('./config/keys');
 const User=require('./models/userModel');
 const refreshToken = require('./config/refreshToken2');
 const request = require('request-promise');
-
+const CronJob = require('cron').CronJob;
 const app = express()
 
 mongoose.connect(keys.mongodb.dbURI,{ useNewUrlParser: true }, function() {
@@ -14,56 +14,55 @@ mongoose.connect(keys.mongodb.dbURI,{ useNewUrlParser: true }, function() {
 User.findOne({codechefId :keys.codechef.username.toLowerCase()}).then(function(currentUser){
 	var myUser=currentUser;
 	console.log("Okay");
-  start(myUser['refreshToken']);
+  var refreshToken = myUser['refreshToken'];
+  job.start(refreshToken);
 });
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+const job = new CronJob('0 * */10 * * *', function(myUserRefreshToken) {
+  var userlist=[];
+  User.find(function(err,docs){
+    // console.log(docs);
+    for(var i=0; i<docs.length; i++)
+    {
+      userlist.push(docs[i]["codechefId"]);
+    }
 
-// Update user's model with data shown on friends page
-async function start(myUserRefreshToken){
-  setInterval(function(){
-    var userlist=[];
-    User.find(function(err,docs){
-      // console.log(docs);
-      for(var i=0; i<docs.length; i++)
-      {
-        userlist.push(docs[i]["codechefId"]);
-      }
+    console.log(userlist);
+    var i=0;
+    recursive();
 
-      console.log(userlist);
-
-      for(var i=0 ; i<userlist.length; i++)
-      {
-        var username=userlist[i];
-        refreshToken.refreshAccessToken(myUserRefreshToken,keys.codechef.username.toLowerCase()).then(function(accessToken){
-          var options = {
-            method: 'GET',
-            uri: 'https://api.codechef.com/users/'+username,
-            headers: {
-               'Accept': 'application/json',
-               'Authorization': 'Bearer ' + accessToken
-            },
-            json: true // Automatically parses the JSON string in the response
-          };
-          request(options)
-          .then(function(result){
-            var rankings  =  result['result']['data']['content']['rankings'];
-            var ratings   =  result['result']['data']['content']['ratings'];
-            var institute =  result['result']['data']['content']['organization'];
-            var band      =  result['result']['data']['content']['band'];
-            User.findOneAndUpdate({'codechefId':username},{$set:{rating:ratings,ranking:rankings,institute:institute,band:band}}, function(err, doc){
-              console.log("Updated "+username);
-            });;
-          });
-         });
-
-         await sleep(1000*60*5);
-      }
-    });
-  },1000*60*60*10);
-}
+    function recursive()
+    {
+      var username=userlist[i];
+      console.log(username);
+      refreshToken.refreshAccessToken(myUserRefreshToken,keys.codechef.username.toLowerCase()).then(function(accessToken){
+        var options = {
+          method: 'GET',
+          uri: 'https://api.codechef.com/users/'+username,
+          headers: {
+             'Accept': 'application/json',
+             'Authorization': 'Bearer ' + accessToken
+          },
+          json: true // Automatically parses the JSON string in the response
+        };
+        request(options)
+        .then(function(result){
+          var rankings  =  result['result']['data']['content']['rankings'];
+          var ratings   =  result['result']['data']['content']['ratings'];
+          var institute =  result['result']['data']['content']['organization'];
+          var band      =  result['result']['data']['content']['band'];
+          User.findOneAndUpdate({'codechefId':username},{$set:{rating:ratings,ranking:rankings,institute:institute,band:band}}, function(err, doc){
+            console.log("Updated "+username);
+            i++;
+            setTimeout(function(){},1000*60*5);
+            if(i<userlist.length)
+              recursive();
+          });;
+        });
+       });
+    }
+  });
+});
 
 const port = process.env.port || 8090;
 
